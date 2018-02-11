58d26e51b21a1c95b2afc5a10d9b85bc980f4198
Mar1nka <sakovich.mandarina@gmail.com>
2018-02-12 01:46:21 +0300

    update styles according to task add  webpack.config.js add postcss.config.js

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: './main.js',
+    output: {
+         path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+         publicPath: '/dist'
+    },
+    watch:true,
+    module: {
+        rules: [
+            {
+                test: /\.css$/,    // определяем тип файлов
+                exclude: /node_modules/, // исключаем из обработки папку node_modules
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                importLoaders: 1,
+                                url: false  // важно для пратльной работы  background: url()
+                            }
+                        },
+                        {
+                            loader: 'postcss-loader', // определяем загрузчик
+                            options: {                // используемые плагины
+                                config: {
+                                    path: 'postcss.config.js'
+                                }
+                            }
+                        }
+                        ]
+                })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('bundle.css')
+    ]
+};
